42d15d1407dddbe210b075dca3e8e30a13ec309c
michaelfehr <michael_fehrgmx.de>
2024-09-06 13:15:34 +0200

    updated

---
+++ app/src/main/java/de/androidcrypto/android_hce_beginner_app/BasicReader.java
@@ -0,0 +1,11 @@
+package de.androidcrypto.android_hce_beginner_app;
+
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+
+public class BasicReader implements NfcAdapter.ReaderCallback {
+    @Override
+    public void onTagDiscovered(Tag tag) {
+
+    }
+}
